cac204e43c7b951f5af2b0d44b53d3dc9bcae370
GitHub Action <action@github.com>
2023-04-02 13:45:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/xjdxjd/myplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/xjdxjd/myplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xjdxjd.myplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xjdxjd/myplugintemplate/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/xjdxjd/myplugintemplate/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xjdxjd.myplugintemplate.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
